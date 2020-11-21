026dc746ea527a931f4947cece57273ba5e1856d
Fernando Pavanello <fernando.pavanello2@gmail.com>
2020-11-21 10:42:28 -0300

    Add treatment to errors

--- routes/numbers.js
+++ routes/numbers.js
@@ -4,8 +4,19 @@ const sanitizedNumber = require('../controller/sanitizeNumber')
 
 router.get('/:rawNumber', (req, res) => {
     const rawNumber = req.params.rawNumber
-    const teste = sanitizedNumber(rawNumber)
-    return res.status(200).json({extenso: teste})
+    try {
+        const teste = sanitizedNumber(rawNumber)
+        return res.status(200).json({extenso: teste})
+    } catch {
+        if (typeof rawNumber !== 'number') {
+            res.status(406).json({message: 'Invalid params, please put a number'})
+        }
+        return res.status(500).json({error: 'API couldn\'t parser the result'})
+    }
+})
+
+router.get('/*', (req, res) => {
+    res.status(406).json({message: 'Unexpected route'})
 })
 
 module.exports = router
