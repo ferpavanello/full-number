b275a3a84ad4a196df9080a30eadc9bc877940e6
Fernando Pavanello <fernando.pavanello2@gmail.com>
2020-11-21 10:22:10 -0300

    Improve API to return the message treated

--- routes/numbers.js
+++ routes/numbers.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const sanitizedNumber = require('../controller/sanitizeNumber')
 
 router.get('/:rawNumber', (req, res) => {
     const rawNumber = req.params.rawNumber
-    console.log('param', rawNumber)
+    const teste = sanitizedNumber(rawNumber)
+    return res.status(200).json({extenso: teste})
 })
 
 module.exports = router
