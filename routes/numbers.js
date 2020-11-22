282169d6223f6f4100280e2b03334292d5838489
Fernando Pavanello <fernando.pavanello2@gmail.com>
2020-11-22 13:20:22 -0300

    Make API treat just valid numbers

--- routes/numbers.js
+++ routes/numbers.js
@@ -5,12 +5,12 @@ const sanitizeNumber = require('../controller/sanitizeNumber')
 router.get('/:rawNumber', (req, res) => {
     const rawNumber = req.params.rawNumber
     try {
-        const sanitizedNumber = sanitizeNumber(rawNumber)
-        return res.status(200).json({extenso: sanitizedNumber})
-    } catch {
         if (isNaN(rawNumber)) {
             return res.status(406).json({message: 'Invalid params, please put a number'})
         }
+        const sanitizedNumber = sanitizeNumber(rawNumber)
+        return res.status(200).json({extenso: sanitizedNumber})
+    } catch {
         return res.status(500).json({error: 'API couldn\'t parser the result'})
     }
 })
